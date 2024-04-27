8babf67a77d058742018826ee98f4797c8ad11e5
Lê Xuân Công <congle301099@gmail.com>
2024-04-27 08:15:55 +0700

    add login register

--- laravel_giua_ky/app/Models/User.php
+++ laravel_giua_ky/app/Models/User.php
@@ -11,6 +11,10 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    public function userProfile()
+    {
+        return $this->hasOne(UserProfile::class);
+    }
     /**
      * The attributes that are mass assignable.
      *
